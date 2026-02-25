d30532c6758ab7a83a80e8d2b1323783bb62f0e6
tureydrip <juliangoza234rg@gmail.com>
2026-02-24 21:09:42 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase, ref, set, get, child, update, push, runTransaction } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyDrNambFw1VNXSkTR1yGq6_B9jWWA1LsxM",
+    authDomain: "clientesvip-be9bd.firebaseapp.com",
+    projectId: "clientesvip-be9bd",
+    storageBucket: "clientesvip-be9bd.firebasestorage.app",
+    messagingSenderId: "131036295027",
+    appId: "1:131036295027:web:3cc360dca16d4873f55f06"
+  };
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// Exportar funciones para usarlas en otros archivos
+export { auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, ref, set, get, child, update, push, runTransaction };
